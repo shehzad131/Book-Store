18e04239f5b222556e48a5f5ae9ffd850881f72a
Ajmal Shehzad <shehzadshah131@gmail.com>
2022-06-29 17:45:26 +0530

    data on database is shown in admin and user pages

--- helpers/book-helpers.js
+++ helpers/book-helpers.js
@@ -1,4 +1,5 @@
 const { ObjectId } = require('mongodb');
+const { default: async } = require('neo-async');
 var db = require('../config/connection')
 
 module.exports = {
@@ -6,6 +7,12 @@ module.exports = {
         //console.log(book);
         db.get().collection('book').insertOne(book).then((data)=>{
             callback(data.insertedId);
+        })
+    },
+    viewAllBooks: ()=>{
+        return new Promise(async(resolve,reject)=>{
+            let bookList = await db.get().collection('book').find().toArray()
+            resolve(bookList)
         })
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -4,32 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  let books = [
-    {
-      'name': "Da Vinci Code",
-      'author': "Dan Brown",
-      'price': "999",
-      'img': "https://picsum.photos/200/300"
-    },
-    {
-      'name': "Harrry Potter",
-      'author': "J K Rowling",
-      'price': "565",
-      'img': "https://picsum.photos/200/301"
-    },
-    {
-      'name': "Sherlock Holmes",
-      'author': "Sir Arthur Conan Doyle",
-      'price': "626",
-      'img': "https://picsum.photos/200/302"
-    },
-    {
-      'name': "Hercule Poirott",
-      'author': "Agatha Christie",
-      'price': "875",
-      'img': "https://picsum.photos/200/303"
-    },
-  ]
   res.render('index', { books, admin: false });
 });
 
